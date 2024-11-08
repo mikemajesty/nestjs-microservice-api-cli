96344ca01d9b259db19e445eef40bb2372ee911c
mike-lima-gaivota <mike.rodrigues.lima@gmail.com>
2024-11-08 18:56:56 -0300

    update cli

--- src/templates/infra/service.js
+++ src/templates/infra/service.js
@@ -1,18 +1,24 @@
 const { dashToPascal } = require("../../textUtils")
 
 const getServiceInfra = (name) => `import { Injectable } from '@nestjs/common';
+import { z } from 'zod';
+
+import { ValidateSchema } from '@/utils/decorators';
 
 import { I${dashToPascal(name)}Adapter } from './adapter';
 
-export type ${dashToPascal(name)}GetInput = {};
-export type ${dashToPascal(name)}GetOutput = {};
+const ${dashToPascal(name)}Schema = z.object({ name: z.string().trim() });
 
 @Injectable()
 export class ${dashToPascal(name)}Service implements I${dashToPascal(name)}Adapter {
+  @ValidateSchema(${dashToPascal(name)}Schema)
   get(input: ${dashToPascal(name)}GetInput): ${dashToPascal(name)}GetOutput {
     return input;
   }
 }
+
+export type ${dashToPascal(name)}GetInput = z.infer<typeof ${dashToPascal(name)}Schema>;
+export type ${dashToPascal(name)}GetOutput = ${dashToPascal(name)}GetInput;
 `
 
 module.exports = {
--- src/templates/libs/service.js
+++ src/templates/libs/service.js
@@ -9,17 +9,16 @@ import { I${dashToPascal(name)}Adapter } from './adapter';
 
 const ${dashToPascal(name)}Schema = z.object({ name: z.string().trim() });
 
-export type ${dashToPascal(name)}Input = z.infer<typeof ${dashToPascal(name)}Schema>;
-
-export type ${dashToPascal(name)}Output = string;
-
 @Injectable()
 export class ${dashToPascal(name)}Service implements I${dashToPascal(name)}Adapter {
   @ValidateSchema(${dashToPascal(name)}Schema)
-  get({ name }: ${dashToPascal(name)}Input): ${dashToPascal(name)}Output {
-    return name;
+  get(input: ${dashToPascal(name)}Input): ${dashToPascal(name)}Output {
+    return input;
   }
 }
+
+export type ${dashToPascal(name)}Input = z.infer<typeof ${dashToPascal(name)}Schema>;
+export type ${dashToPascal(name)}Output = ${dashToPascal(name)}Input;
 `
 
 module.exports = {
